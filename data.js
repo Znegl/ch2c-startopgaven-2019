0ac018d27d558a7e4d41bd5dd0a005d0b2d2af58
Thomas Grinderslev <thomas@edulab.dk>
2019-09-21 16:46:38 +0200

    Added dummy data for the last answer

--- data.js
+++ data.js
@@ -2,5 +2,6 @@ module.exports = {
     '1': 'et',
     to: '..',
     '...': '---',
-    '----': 'granatæble'
+    '----': 'granatæble',
+    'last': '<span class="hurra">Tillykke, I har løst startopgaven!<br><br>Vi starter på Skagen Havn 🚢</span><style>.hurra {display: inline-block; font-size: .75em; line-height: 1.1em;} form > br, input, label, button {display: none;}</style>'
 }
