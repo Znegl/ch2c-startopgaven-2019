d93584f4d618ab77350b7787a0a3992c55c70667
Thomas Grinderslev <thomas@edulab.dk>
2019-09-21 17:00:42 +0200

    Added a little more data

--- data.js
+++ data.js
@@ -1,7 +1,11 @@
 module.exports = {
     '1': 'et',
     to: '..',
-    '...': '---',
-    '----': 'granatæble',
+    '...': 'pilt',
+    'væbner': 'trop',
+    'klan': 'fråseri',
+    'vrede': 'du må ikke bryde ægteskabet',
+    'du må ikke stjæle': 'uranus',
+    'neptun': '✋',
     'last': '<span class="hurra">Tillykke, I har løst startopgaven!<br><br>Vi starter på Skagen Havn 🚢</span><style>.hurra {display: inline-block; font-size: .75em; line-height: 1.1em;} form > br, input, label, button {display: none;}</style>'
 }
