fac48984601b171fd8562bc37ab7ef29eb3fcf66
Thomas Grinderslev <thomas@edulab.dk>
2019-09-28 19:19:06 +0200

    Fixed a bug where HTML would end up in the page title

--- build.js
+++ build.js
@@ -16,7 +16,16 @@ const buildPromises = pages.map(([pageName, pageContent]) => {
   const fileDir = `${buildDir}/${pageName.toLowerCase().trim()}`
   const filePath = `${fileDir}/index.html`
 
+  // Remove all style tags and HTML formatting from the page content to get the page title
+  // Also remove everything after the first <br> tag
+  const pageTitle = pageContent.replace(/(<style.+<\/style>)|(<br.*)|(<[^>]*>)/g, '')
+
   return fsPromises.mkdir(fileDir).then(() => {
-    return fsPromises.writeFile(filePath, template.replace(/\{\{(pageTitle|pageContent)\}\}/g, pageContent))
+    return fsPromises.writeFile(
+      filePath,
+      template
+        .replace(/\{\{pageContent\}\}/g, pageContent)
+        .replace(/\{\{pageTitle\}\}/g, pageTitle)
+    )
   })
 })
