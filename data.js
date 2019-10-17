b5f6b1a9536b2ab88929ab0ec15a3057af4aad26
Thomas Grinderslev <thomas@edulab.dk>
2019-10-17 15:17:41 +0200

    Updated YouTubers again

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ module.exports = {
     'iota': 'oktober',
     'november': 'knægt',
     'dame': 'Thea Gaming',
-    'กินไปไทยหรั่ง': 'Aluminium',
+    'AquaHoldet': 'Aluminium',
     'Silicium': 'Livet i havet',
     'Livet på land': 'O',
     'P': '<span class="small">Otto Joachim Moltke</span>',
